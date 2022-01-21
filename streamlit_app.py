675069644ac9236182e0cbb237b89903107b2c15
Aswin <hi.aswin@gmail.com>
2022-01-20 18:16:53 -0800

    Calculating

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,10 @@ print(platform.dist())
 with open("/proc/cpuinfo", "r")  as f:
     info = f.readlines()
 
+cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
+for index, item in enumerate(cpuinfo):
+    print("    " + str(index) + ": " + item)
+    
 n = 10000
 m = 4000
 np.random.seed(100)
